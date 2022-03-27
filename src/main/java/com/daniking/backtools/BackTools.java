9881a29421d9661987e7cd5cdfdfacd05f1b09d5
DanikingRD <danikingrd@gmail.com>
2022-03-27 07:49:30 -0400

    update version

--- src/main/java/com/daniking/backtools/BackTools.java
+++ src/main/java/com/daniking/backtools/BackTools.java
@@ -11,7 +11,7 @@ import java.util.function.Supplier;
 public class BackTools implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(BackTools.class);
-	public static final String VERSION = "1.18.2-1";
+	public static final String VERSION = "1.18.2-2";
 
 	@Override
 	public void onInitialize() {
