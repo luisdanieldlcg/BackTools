eab17c7b0519c24c6607447155175e533b866803
FireInstall <kettnerl@hu-berlin.de>
2024-02-04 20:18:18 +0100

    Added offset to the config offset and orientation now allow optional nbt to further specify an item offset and orientation now allow optional x and y axis offset/rotation

---
+++ src/main/java/com/daniking/backtools/ToolConfig.java
@@ -0,0 +1,56 @@
+package com.daniking.backtools;
+
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.ComponentType;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
+
+public class ToolConfig {
+    private final static ToolConfig EMPTY = new ToolConfig(null, 0, 0, 0);
+
+    private final float x, y, z;
+    private final @Nullable ComponentMap components;
+
+    public ToolConfig(@Nullable ComponentMap componentsToMatch, float x, float y, float z) {
+        this.components = componentsToMatch;
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static ToolConfig empty() {
+        return EMPTY;
+    }
+
+    public boolean doComponentsMatch(@Nullable ComponentMap toCheck) {
+        if (components == null) {
+            return true;
+        }
+        if (toCheck == null) {
+            return false;
+        }
+
+        for (ComponentType<?> key : components.getTypes()) {
+            if (!Objects.equals(components.get(key), toCheck.get(key))) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getZ() {
+        return z;
+    }
+}
