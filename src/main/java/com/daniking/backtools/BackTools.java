442627d10cc063bc099db3f2d3c081692223e78c
luisdanieldlcg <luisdanieldlcg@gmail.com>
2025-02-06 09:00:15 -0400

    Bump to version 1.20.4-1

--- src/main/java/com/daniking/backtools/BackTools.java
+++ src/main/java/com/daniking/backtools/BackTools.java
@@ -11,7 +11,7 @@ import java.util.function.Supplier;
 public class BackTools implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(BackTools.class);
-	public static final String VERSION = "1.20-2";
+	public static final String VERSION = "1.20.4-1";
 
 	@Override
 	public void onInitialize() {
